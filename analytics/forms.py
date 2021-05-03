5d241f91149bdb33b17a6353194a8e0a484f213f
Yerson Alexander Arredondo García <yerson@veredata.co>
2021-05-03 10:34:09 -0500

    Validate TestUser form

--- analytics/forms.py
+++ analytics/forms.py
@@ -30,6 +30,16 @@ class TestUserForm(forms.Form):
     edad = forms.IntegerField(required=False)
 
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        email_taken = TestUser.objects.filter(email=email).exists()
+
+        if email_taken:
+            raise forms.ValidationError('El email esta en uso.')
+
+        return email
+
+
     def save(self):
         data = self.cleaned_data
 
