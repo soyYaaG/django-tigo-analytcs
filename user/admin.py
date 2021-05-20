56a4ab96500ac3971b38f8587f78a6461b270f20
Yerson Alexander Arredondo García <yerson@veredata.co>
2021-05-20 10:34:40 -0500

    Add user in site admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Models
+from user.models import User
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'is_staff',
+        'is_active'
+    )
+
+    list_editable = ('first_name', 'last_name')
+
+    search_fields = ('email', 'username')
+
+    list_filter = ('created', 'modified', 'is_active')
+
+    ordering = ('first_name', '-email')
+
+
+admin.site.register(User, UserAdmin)
